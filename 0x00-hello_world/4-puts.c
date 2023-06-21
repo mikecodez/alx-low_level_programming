bea68096f3db3a5ede05b4ef659f1e6c47209af3
mikecodez <artofmike96@gmail.com>
2023-06-21 01:37:46 -0700

    A C program using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main()
 {
-    puts("Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
 
     getchar();
     return 0;
