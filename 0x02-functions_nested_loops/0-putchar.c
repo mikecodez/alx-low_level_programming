bf660ff8a2dc07c3fcd9f4b637813ca894c546a3
mikecodez <artofmike96@gmail.com>
2023-06-21 07:43:05 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * mian - program that prints __putchar
+ * followed by a new line
+ * 
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+putchar("_putchar\n");
+return (0);
+
+}
