c5e0bd59e9eb78fb968f9c9041bd83f6853164f6
mikecodez <artofmike96@gmail.com>
2023-06-21 01:39:47 -0700

    A C program using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 /* A C-program that uses the puts function to write a string */
 #include <stdio.h>
-int main()
+int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
-
     getchar();
     return 0;
-
 }
-
