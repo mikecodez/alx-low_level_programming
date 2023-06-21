2e3430c34ea1d65d1c7761c16c72439431d2cb7d
mikecodez <artofmike96@gmail.com>
2023-06-21 15:15:14 -0700

    0-putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,8 @@
-#include <stdio.h>
-
-/**
- * main -- main h file
- *
- * Return: always 0
- */
-
-int main(void)
-{
-
-}
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last__digits(int);
