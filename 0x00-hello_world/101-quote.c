f00ee69434e492af00ea4057a56044ba2fb1f307
mikecodez <artofmike96@gmail.com>
2023-06-21 03:10:11 -0700

    1011-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Program prints exactly
+ * and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * followed by a new line
+ * Return: Always 1
+ *
+ */
+
+int main(void)
+{
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
